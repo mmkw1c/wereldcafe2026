0ded82c231496ff67cb87f003790fec13c7e181b
mmkw1c <m.mukrab@kw1c.nl>
2026-03-17 16:27:36 +0100

    Update main.js
    changed target name

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ async function startAR() {
     // Init MindAR
     const mindarThree = new MindARThree({
       container: app,
-      imageTargetSrc: './public/targets/targets.mind',
+      imageTargetSrc: './public/targets/wereldcafe.mind',
     });
 
     const { renderer, scene, camera } = mindarThree;
