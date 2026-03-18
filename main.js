25550abc15d852444f3c970a5d924f35d4f5322e
mmkw1c <m.mukrab@kw1c.nl>
2026-03-18 19:52:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -166,7 +166,7 @@ async function startAR() {
         // JOUW GETUNDE WAARDES HIER
         // afkomstig uit preview
         // ================================
-        wrapper.position.set(0.000, 0.000, -0.098);
+        wrapper.position.set(0.000, -0.500, -0.098);
         wrapper.rotation.set(-1.388, 3.142, 1.138);
         wrapper.scale.set(0.133, 0.133, 0.133);
 
