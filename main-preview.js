e6ea1070365b43640a3046a0d0bc542c76025683
mmkw1c <m.mukrab@kw1c.nl>
2026-03-18 19:04:28 +0100

    Update main-preview.js
    edit transform 3d model

--- main-preview.js
+++ main-preview.js
@@ -310,9 +310,9 @@ async function startPreview() {
       (gltf) => {
         const model = gltf.scene;
 
-        model.scale.set(0.25, 0.25, 0.25);
-        model.position.set(0, 0, 0);
-        model.rotation.set(0, -Math.PI / 2, 0);
+model.position.set(0.000, 0.000, -0.098);
+model.rotation.set(-1.388, 3.142, 1.138);
+model.scale.set(0.133, 0.133, 0.133);
 
         model.traverse((child) => {
           if (child.isMesh) {
