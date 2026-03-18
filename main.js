aeabe1079376ff48329bf742fd29cfb7e614a36e
mmkw1c <m.mukrab@kw1c.nl>
2026-03-18 01:04:18 +0100

    Update main.js
    testing with alternative model

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ async function startAR() {
     let modelLoaded = false;
 
     loader.load(
-      'https://elektrafungi.com/kw1c/model.glb',
+      'https://elektrafungi.com/kw1c/kerstgroet.glb',
       (gltf) => {
         modelLoaded = true;
 
